8e6347b908ec737ef49b48e922eaaa0442dc4e5c
BobLiu <bobliu@bobliu.tech>
2026-01-04 11:24:43 +0800

    feat: add video ad (#5)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,33 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
     modules: ['@nuxt/eslint', '@element-plus/nuxt'],
+    vue: {
+        compilerOptions: {
+            isCustomElement: (tag) => tag.startsWith('adflux-'),
+        },
+    },
     elementPlus: {
         icon: 'ElIcon',
     },
     css: ['~/assets/css/main.css'],
     ssr: false,
+    app: {
+        head: {
+            meta: [
+                {
+                    name: 'adflux-verification',
+                    content: process.env.ADFLUX_VERIFICATION,
+                },
+            ],
+            script: [
+                {
+                    src: 'https://adflux.bobliu.tech/ads/main.js',
+                    type: 'module',
+                    tagPosition: 'bodyClose',
+                },
+            ],
+        },
+    },
     runtimeConfig: {
         dbUrl: process.env.DATABASE_URL,
         jwtSecret: process.env.JWT_SECRET,

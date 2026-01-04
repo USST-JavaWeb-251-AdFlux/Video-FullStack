a170e23f2d011c6ec8a04c7b552f42b36c9a9e0a
BobLiu <bobliu@bobliu.tech>
2026-01-04 21:57:20 +0800

    feat: add more categories

--- app/app.config.ts
+++ app/app.config.ts
@@ -5,5 +5,12 @@ export default defineAppConfig({
         { label: '娱乐', value: 'entertainment' },
         { label: '音乐', value: 'music' },
         { label: '教育', value: 'education' },
+        { label: '动漫', value: 'anime' },
+        { label: '游戏', value: 'gaming' },
+        { label: '运动', value: 'sports' },
+        { label: '美食', value: 'food' },
+        { label: '旅游', value: 'travel' },
+        { label: '影视', value: 'movie' },
+        { label: '资讯', value: 'news' },
     ],
 });
